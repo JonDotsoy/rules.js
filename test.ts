aaf3d37783d8db9504eb005c488a4f1e7279aabf
Jonathan Delgado <hi@jon.soy>
2022-07-20 16:28:22 -0400

    test: update test

--- test.ts
+++ test.ts
@@ -8,12 +8,10 @@ interface Token {
 const rules = createRules({
   pacman: (token: Token) => token.role === "admin",
   "pacman2:create": (token: Token) => token.role === "admin",
-  "err": (_token: Token) => { throw new Error('fail') },
 });
 
 strictEqual(true, evaluate(rules, 'pacman', { role: "admin" }));
 strictEqual(false, evaluate(rules, 'pacman', { role: "guest" }));
 strictEqual(true, evaluate(rules, 'pacman2:create', { role: "admin" }));
-strictEqual(false, evaluate(rules, 'err', { role: "admin" }));
 
 console.log('[all ok]');
